f18f524cd3899919a99f16bd9659e357a44f1e9f
Arne Voss <arnevoss@pm.me>
2021-11-10 19:56:18 +0100

    Finished design mockup

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 
 #include <curses.h>
 #include <locale.h>
+#include <string>
 
 // TODO:
 // This is just a design mockup. Put all the curses stuff
@@ -77,16 +78,54 @@ void drawPitch(int row, int columns)
 {
     // If too small for slider => just draw text
     // If too small for text => bad luck
+
+    // 12 fixed blocks
+    int cols_temp = columns - 12;
+    int cols_tempdiv = cols_temp / 8;
+    int cols_temp_mod = (cols_temp % 8) / 2;
+
+
+    mvaddstr(row, cols_temp_mod, "-50");
+    for(int i = 0; i < 5; ++i)
+    {
+        mvaddch(row, cols_temp_mod+3+cols_tempdiv*i, '|');
+    }
+
+    attron(A_BOLD);
+    mvaddstr(row, cols_temp_mod+3+cols_tempdiv*4+1, "C#0");
+    attroff(A_BOLD);
+
+    int newstart = cols_temp_mod+3+cols_tempdiv*4+4;
+    for(int i = 0; i < 5; ++i)
+    {
+        mvaddch(row, newstart+cols_tempdiv*i, '|');
+    }
+    mvaddstr(row, newstart+cols_tempdiv*4+1, "+50");
+
+    attron(A_BOLD);
+    mvaddch(row+1, cols_temp_mod+3+cols_tempdiv*4+2, '^');
+    attroff(A_BOLD);
 }
 
 void drawInput(int row, int columns)
 {
+    int cols_left = 0.25F * columns;
+    int cols_middle = 0.5F * columns - 5;
+    int cols_right = 0.75F * columns;
 
+    mvaddch(row, cols_left, '<');
+    mvaddch(row, cols_right, '>');
+
+    mvaddstr(row, cols_middle, "INPUT #0");
 }
 
 void drawOptions(int row, int columns)
 {
+    move(row, 0);
 
+    addstr("QQuit ");
+    addstr("LeftPrevious Input ");
+    addstr("RightPrevious Input ");
 }
 
 void draw()
@@ -98,9 +137,9 @@ void draw()
     getmaxyx(stdscr, rows, columns);
 
     int title = 0;
-    int pitch = 0.5F * rows;
-    int input = 0.75F * rows;
-    int options = rows;
+    int pitch = 0.33F * rows;
+    int input = 0.66F * rows;
+    int options = rows-1;
 
     drawTitle(title, columns);
     drawPitch(pitch, columns);
